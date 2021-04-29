90cc5a2e77bff0ebdfe97531864ae45db87c957b
PietroFreire <freiregabriel98@gmail.com>
2021-04-29 14:02:32 -0300

    ✨ Atualizações na GUI inicial

--- Jogo.py
+++ Jogo.py
@@ -3,18 +3,33 @@ import pygame
 import sys
 
 pygame.init()
-screen = pygame.display.set_mode((626, 325), 0, 32)
+screen = pygame.display.set_mode((1000, 625), 0, 32)
 pygame.display.set_caption('WEIGHTLIFTER - Jogos Digitais')
-
-background_image_filename = 'background.jpg'
-title_image_filename = 'title.png'
-
 clock = pygame.time.Clock()
+
+#---------[IMAGENS]---------#
+background_image_filename = 'background.jpg'
+title_image_filename = 'title.svg'
+volume_image_filename = 'volume.svg'
+mutado_image_filename = 'mute.svg'
 
 background = pygame.image.load(background_image_filename).convert()
 title = pygame.image.load(title_image_filename).convert_alpha()
+volume = pygame.image.load(volume_image_filename).convert_alpha()
+mutado = pygame.image.load(mutado_image_filename).convert_alpha()
 
+volume_alpha = 1000
+mutado_alpha = 0
+#---------------------------#
 
+#---------[SONS]---------#
+pygame.mixer.init()
+pygame.mixer.music.load('menu.mp3')
+pygame.mixer.music.play(-1)
+som_on = True
+#------------------------#
+
+#---------[TEXTO]---------#
 fonte = pygame.font.SysFont(None, 30)
 
 
@@ -23,6 +38,7 @@ def texto(texto, fonte, cor, surface, x, y):
     texto_posi = texto.get_rect()
     texto_posi.topleft = (x, y)
     surface.blit(texto, texto_posi)
+#-------------------------#
 
 
 click = False
@@ -32,29 +48,39 @@ def gui():
     while True:
 
         screen.fill((0, 0, 0))
-        screen.blit(title, (105, 1))
+        screen.blit(background, (0, 0))
+        screen.blit(title, (-470, 105))
 
-        mx, my = pygame.mouse.get_pos()
+        mouse_x, mouse_y = pygame.mouse.get_pos()
 
-        b_jogar = pygame.Rect(250, 100, 120, 40)
-        b_como_jogar = pygame.Rect(250, 160, 120, 40)
-        b_sair = pygame.Rect(250, 225, 120, 40)
-        if b_jogar.collidepoint((mx, my)):
+        b_mute = pygame.Rect(925, 15, 50, 50)
+        b_jogar = pygame.Rect(440, 250, 150, 50)
+        b_como_jogar = pygame.Rect(440, 325, 150, 50)
+        b_sair = pygame.Rect(440, 400, 150, 50)
+        if b_mute.collidepoint((mouse_x, mouse_y)):
+            if click:
+                mute(som_on)
+        if b_jogar.collidepoint((mouse_x, mouse_y)):
             if click:
                 jogo()
-        if b_como_jogar.collidepoint((mx, my)):
+        if b_como_jogar.collidepoint((mouse_x, mouse_y)):
             if click:
                 como_jogar()
-        if b_sair.collidepoint((mx, my)):
+        if b_sair.collidepoint((mouse_x, mouse_y)):
             if click:
                 pygame.quit()
                 sys.exit()
-        pygame.draw.rect(screen, (178, 35, 35, 30), b_jogar)
-        pygame.draw.rect(screen, (178, 35, 35), b_como_jogar)
-        pygame.draw.rect(screen, (178, 35, 35), b_sair)
-        texto('Jogar', fonte, (255, 255, 255), screen, 280, 110)
-        texto('Como Jogar', fonte, (255, 255, 255), screen, 252, 170)
-        texto('Sair', fonte, (255, 255, 255), screen, 285, 235)
+        pygame.draw.rect(screen, (0, 0, 0), b_mute, border_radius=100)
+        pygame.draw.rect(screen, (0, 0, 0), b_jogar, border_radius=15)
+        pygame.draw.rect(screen, (0, 0, 0), b_como_jogar, border_radius=15)
+        pygame.draw.rect(screen, (0, 0, 0), b_sair, border_radius=15)
+        volume.set_alpha(volume_alpha)
+        screen.blit(volume, (709, -200))
+        mutado.set_alpha(mutado_alpha)
+        screen.blit(mutado, (725, -185))
+        texto('Jogar', fonte, (255, 255, 255), screen, 485, 265)
+        texto('Como Jogar', fonte, (255, 255, 255), screen, 455, 340)
+        texto('Sair', fonte, (255, 255, 255), screen, 493, 415)
 
         click = False
         for event in pygame.event.get():
@@ -71,6 +97,23 @@ def gui():
 
         pygame.display.update()
         clock.tick(60)
+
+
+def mute(status):
+    global som_on
+    global volume_alpha
+    global mutado_alpha
+
+    if status == True:
+        pygame.mixer.music.set_volume(0)
+        mutado_alpha = 1000
+        volume_alpha = 0
+        som_on = False
+    elif status == False:
+        pygame.mixer.music.set_volume(100)
+        mutado_alpha = 0
+        volume_alpha = 1000
+        som_on = True
 
 
 def jogo():
@@ -109,5 +152,6 @@ def como_jogar():
 
         pygame.display.update()
         clock.tick(60)
+
 
 gui()
